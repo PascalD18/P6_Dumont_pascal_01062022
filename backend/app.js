e8fc657e55453884ce3984af38cbbb4455e7e852
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-06-25 13:54:11 +0200

    --- Auto-Git Commit ---

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  // Connexion de la base de données 'mongoose' - Projet PASCAL DUMONT 18 - Cluster0
+mongoose.connect('mongodb+srv://Dpascal18-2:Mdpmondodbdpascal18-22022@cluster0.tqjqjtr.mongodb.net/?retryWrites=true&w=majority',
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
