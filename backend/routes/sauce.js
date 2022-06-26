58a66a400c2933bfb347bd5d88e9c3a5fc0239d4
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-06-26 21:57:23 +0200

    Maj

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const sauceCtrl = require('../controllers/sauce');
-//const auth = require('../midleware/auth')
+const auth = require('../midleware/auth')
 
-router.post('/',sauceCtrl.createThing);
-router.put('/:id',sauceCtrl.modifyThing);
-router.delete('/:id',sauceCtrl.deleteThing);
-router.get('/:id',sauceCtrl.getOneThing);
+router.post('/',auth,sauceCtrl.createThing);
+router.put('/:id',auth,sauceCtrl.modifyThing);
+router.delete('/:id',auth,sauceCtrl.deleteThing);
+router.get('/:id',auth,sauceCtrl.getOneThing);
 router.get('/',sauceCtrl.getAllThings);
 
 module.exports = router;
