d33cac9a6d147330445a575c215e6568954d95bb
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-06-30 10:12:27 +0200

    Maj

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const SauceSchema = mongoose.Schema({
+    userId:{type:String,require :true},
     name: { type: String, require: true },
     manufacture: { type: String, require: true },
     description: { type: String, require: true },
     mainPepper: { type: String, require: true },
     imageUrl: { type: String, require: false},
-    heat: { type: Number, require: true, default:0 },
+    heat: { type: Number, require: true, default:1 },
     likes: { type: Number, require: true ,default:0},
     dislikes: { type: Number, require: true ,default:0},
     usersLiked: { type: [String], require: true,default:[] },
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,12 +5,12 @@ const multer = require('../midleware/multer-config')
 
 const router = express.Router();
 
-router.post('/', auth, multer,sauceCtrl.createSauce);
-router.put('/:id', auth,multer,sauceCtrl.modifySauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 
-router.delete('/:id', auth,sauceCtrl.deleteSauce);
-router.get('/:id', auth,sauceCtrl.getOneSauce);
-router.get('/', auth,sauceCtrl.getAllSauces);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
 
 //router.post('/:id/like', auth,sauceCtrl.createSauce);
 //router.put('/:id/like', auth,sauceCtrl.modifySauce);
