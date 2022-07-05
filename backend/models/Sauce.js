3c73b53c0176587fc67b895383676cc2178a8235
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-07-05 15:49:17 +0200

    --- Auto-Git Commit ---

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,17 +1,25 @@
 const mongoose = require('mongoose');
 
-const SauceSchema = mongoose.Schema({
-    userId: { type: String, require: true },
-    name: { type: String, require: true },
-    manufacturer: { type: String, require: true },
-    description: { type: String, require: true },
-    mainPepper: { type: String, require: true },
-    imageUrl: { type: String, require: false },
-    heat: { type: Number, require: true, default: 1 },
-    likes: { type: Number, require: true, default: 0 },
-    dislikes: { type: Number, require: true, default: 0 },
-    usersLiked: { type: [String], require: true, default: [] },
-    usersDisliked: { type: [String], require: true, default: [] },
-});
+// const SauceSchema = mongoose.Schema({
+const Sauce = mongoose.model(
+    "test",
+    {
+        userId: { type: String, require: true },
+        name: { type: String, require: true },
+        manufacturer: { type: String, require: true },
+        description: { type: String, require: true },
+        mainPepper: { type: String, require: true },
+        imageUrl: { type: String, require: false },
+        heat: { type: Number, require: true, default: 1 },
+        likes: { type: Number, require: true, default: 0 },
+        dislikes: { type: Number, require: true, default: 0 },
+        usersLiked: { type: [String], require: true, default: [] },
+        usersDisliked: { type: [String], require: true, default: [] },
+    },
+    "sauces"
+)
 
-module.exports = mongoose.model('Sauce', SauceSchema);
+//});
+
+//module.exports = mongoose.model('Sauce', SauceSchema);
+module.exports = {Sauce};
