11d2711904168b056eb51e14b5a1f155dc40348b
Dumont Pascal <93477784+PascalD18@users.noreply.github.com>
2022-07-18 19:17:02 +0200

    Maj

--- backend/midleware/dataSauce.js
+++ backend/midleware/dataSauce.js
@@ -2,19 +2,19 @@
 const { deleteSauce } = require('../controllers/sauce');
 const Sauce = require('../models/Sauce');
 exports.manufacturer = (req, res, next) => {
-  //  var sauceObject = JSON.parse(req.body.sauce);
+    //  var sauceObject = JSON.parse(req.body.sauce);
     // Création du formulaire de la sauce dans l'objet 'sauce'
     Sauce.findOne({ _id: req.params.id })
-        .then(sauce => {
+        .then(() => {
 
             // Vérifie la saisie de manufacturer
             let valideDataSauce = new RegExp(/^[a-z-A-Z]+$/);
             if (valideDataSauce.test(req.body.manufacturer)) {
                 next();
             } else {
-                
-            //Reponse de l'erreur si la saisie contient des chiffres
-             return res.status(400).json({ message: `Ne saisir que des lettres pour le champs 'manufacturer'. ` });
+
+                //Reponse de l'erreur si la saisie contient des chiffres
+                return res.status(400).json({ message: `Ne saisir que des lettres pour le champs 'manufacturer'. ` });
             }
         })
         .catch(error => res.status(400).json({ error }))
